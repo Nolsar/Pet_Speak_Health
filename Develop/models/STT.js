b7b2d3a8856ceda7a204c2c4833d83f3ea301654
ESMAMU1986 <martinez.martin.mr@gmail.com>
2021-03-28 01:55:52 -0400

    adjusted some code that was missing async and now needing a session ID

--- Develop/models/STT.js
+++ Develop/models/STT.js
@@ -1,26 +1,46 @@
 const fs = require('fs');
 const util = require('util');
-const {struct} = require('pb-util');
+const { struct } = require('pb-util');
 // Imports the Dialogflow library
 const dialogflow = require('@google-cloud/dialogflow');
+const uuid = require('uuid');
+const projectId = "core-outcome-308917";
+// A unique identifier for the given session
+const sessionId = uuid.v4();
 
 // Instantiates a session client
 const sessionClient = new dialogflow.SessionsClient();
 
-// The path to identify the agent that owns the created intent.
-const sessionPath = sessionClient.projectAgentSessionPath(
+async function detectIntent(
   projectId,
   sessionId
-);
+ // query,
+ // contexts,
+ // languageCode
+) 
+{
+  // The path to identify the agent that owns the created intent.
+ const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  )
+};
+
+
+// The path to identify the agent that owns the created intent.
+//const sessionPath = sessionClient.projectAgentSessionPath(
+//  projectId,
+//  sessionId
+//);
 
 // Read the content of the audio file and send it as part of the request.
 const readFile = util.promisify(fs.readFile);
-const inputAudio = await readFile("..\Develop\models\STT.js"); //replace with file mp3
+const inputAudio = async () => await readFile("../public/audio.wav"); //replace with file mp3
 const request = {
   session: sessionPath,
   queryInput: {
     audioConfig: {
-      audioEncoding: encoding, //replace 23, 24, 25
+      audioEncoding: "enum", //replace 23, 24, 25
       sampleRateHertz: 16000, //1600 hrz as recommended by documentation
       languageCode: 'en-us', //english
     },
@@ -29,8 +49,8 @@ const request = {
 };
 
 // Recognizes the speech in the audio and detects its intent.
-const [response] = await sessionClient.detectIntent(request);
-
+const response = async () => await sessionClient.detectIntent(request);
+console.log(response.queryResult);
 console.log('Detected intent:');
 const result = response.queryResult;
 // Instantiates a context client
